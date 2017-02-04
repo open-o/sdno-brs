1b2ab903603381a0775fc41ac78782e52aae3ff7
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2017-02-04 15:45:17 +0800

    Fix query parameter escape character problem
    getParameterMap function converts character '/' to '%2F', this will occur problem.
    
    SDNO-1142
    
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
    Change-Id: I89116588195b839cde11c3ab0bbd3db6df4fd5fb

--- br-svc/service/src/main/java/org/openo/sdno/brs/resource/LogicalTPResource.java
+++ br-svc/service/src/main/java/org/openo/sdno/brs/resource/LogicalTPResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package org.openo.sdno.brs.resource;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -100,8 +102,10 @@ public class LogicalTPResource extends IResource<LogicalTPService> {
     @GET
     @Produces("application/json")
     @Consumes("application/json")
-    public Object getLogicalTerminationPointList(@Context HttpServletRequest request) throws ServiceException {
-        String queryString = request.getQueryString();
+    public Object getLogicalTerminationPointList(@Context HttpServletRequest request)
+            throws ServiceException, UnsupportedEncodingException {
+        // TODO: need to solve query parameter contains key words: "=" or "&" problem
+        String queryString = URLDecoder.decode(request.getQueryString(), "UTF-8");
         return service.getLogicalTPs(queryString, LOGICAL_TPS);
     }
 
