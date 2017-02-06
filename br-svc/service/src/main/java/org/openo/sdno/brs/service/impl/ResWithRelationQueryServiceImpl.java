2f16fd311f18a6246be6526aeaed9efcd6b2d62f
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2017-02-06 14:28:53 +0800

    Fix unMarshal "{}" to null object problem
    "{}" can be unmarshaled to any type object whose attributes are all null, this will occur problem.
    
    SDNO-894
    
    Change-Id: If055f639f12e2c76944852b866c8382723e5848b
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>

--- br-svc/service/src/main/java/org/openo/sdno/brs/service/impl/ResWithRelationQueryServiceImpl.java
+++ br-svc/service/src/main/java/org/openo/sdno/brs/service/impl/ResWithRelationQueryServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,7 @@ public class ResWithRelationQueryServiceImpl implements ResWithRelationQueryServ
 
         T result = (T)HttpResponseUtil.assembleRspData(response.getResponseContent(), classType);
 
-        if(!StringUtils.isEmpty(result.getId())) {
+        if(null != result && !StringUtils.isEmpty(result.getId())) {
             List<T> resourceLst = new ArrayList<T>();
             resourceLst.add(result);
             initRelationInfoMap(classType);
--- br-svc/service/src/main/java/org/openo/sdno/brs/util/http/HttpResponseUtil.java
+++ br-svc/service/src/main/java/org/openo/sdno/brs/util/http/HttpResponseUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -179,7 +179,12 @@ public class HttpResponseUtil {
         // If there is object field, it is a single modify or query operation.
         // Structure is {"object":{}}.
         if(map.containsKey(Constant.OBJECT_KEY)) {
-            return HttpRelationUtil.transferDataModel((Map)map.get(Constant.OBJECT_KEY), type);
+            Map objMap = (Map)map.get(Constant.OBJECT_KEY);
+            if(objMap.isEmpty()) {
+                LOGGER.info("No object in object map");
+                return null;
+            }
+            return HttpRelationUtil.transferDataModel(objMap, type);
         }
 
         Object obj = null;
