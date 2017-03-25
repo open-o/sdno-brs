801a3dfe22c3a307421dc4f8fea5d20a2c160db6
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2017-03-25 17:34:09 +0800

    Refresh Brs Swagger API document
    Refresh Brs Swagger API document,use maven to generate swagger json and deploy to service directory.
    
    SDNO-1476
    
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
    Change-Id: Ic655c02979e6e8cba68f4c24ed6538ba30ed30bc

--- br-svc/service/src/main/java/org/openo/sdno/brs/resource/SwaggerRoa.java
+++ br-svc/service/src/main/java/org/openo/sdno/brs/resource/SwaggerRoa.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.openo.sdno.brs.resource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.ws.rs.GET;
@@ -24,6 +25,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.io.IOUtils;
+import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Swagger API Doc.<br/>
@@ -34,16 +38,31 @@ import org.apache.commons.io.IOUtils;
 @Path("/sdnobrs/v1")
 @Produces({MediaType.APPLICATION_JSON})
 public class SwaggerRoa {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerRoa.class);
+
+    private String swaggerFilePath;
+
+    public void setSwaggerFilePath(String swaggerFilePath) {
+        this.swaggerFilePath = swaggerFilePath;
+    }
+
     /**
-     * API doc.
-     * @param filename
-     * @return
-     * @throws IOException
+     * Query api document.<br>
+     * 
+     * @return api document
+     * @throws ServiceException when read document failed
+     * @since SDNO 0.5
      */
     @GET
     @Path("/swagger.json")
-    public String apidoc() throws IOException{
-        ClassLoader classLoader = getClass().getClassLoader();
-        return IOUtils.toString(classLoader.getResourceAsStream("swagger.json"));
+    @Produces({MediaType.APPLICATION_JSON})
+    public String apidoc() throws ServiceException {
+        try (FileInputStream finStream = new FileInputStream(swaggerFilePath)) {
+            return IOUtils.toString(finStream);
+        } catch(IOException e) {
+            LOGGER.error("Read swagger json occurs exception");
+            throw new ServiceException("Read swagger json occurs exception");
+        }
     }
 }
